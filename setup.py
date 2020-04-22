c2b83d569d03027c3837d4a35fc44cbea5e1bd9e
Jun Zhang <zhangjun990222@qq.com>
2020-04-22 19:30:59 +0800

    更新版本号

--- setup.py
+++ setup.py
@@ -5,14 +5,15 @@ from setuptools import setup, find_packages
 
 setup(
     name='librecframework',
-    version='1.0',
+    version='1.1',
     packages=find_packages(),
     install_requires=[
         'numpy>=1.17.2',
         'scipy>=1.3.1',
         'visdom==0.1.8.9',
         'setproctitle>=1.1.10',
-        'torch>=1.2.0'
+        'torch>=1.2.0',
+        'nvidia_ml_py3==7.352.0'
     ],
     author='Jun Zhang',
     author_email='zhangjun990222@qq.com',
