64a17ae400aa273f82ffd3ee46f0a0bd1c3dcb40
Jun Zhang <zhangjun990222@qq.com>
2021-12-17 13:50:22 +0800

    fix FileNotFoundError bug

--- setup.py
+++ setup.py
@@ -5,7 +5,7 @@ from setuptools import setup, find_packages
 
 setup(
     name='librecframework',
-    version='1.3.2',
+    version='1.3.3',
     packages=find_packages(),
     python_requires='>=3.8',
     install_requires=[
